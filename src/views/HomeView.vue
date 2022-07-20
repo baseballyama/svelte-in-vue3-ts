b51e32ede47c6ec900e70fc2a8a87419d271d053
Yuichiro Yamashita <xydybaseball@gmail.com>
2022-07-20 23:53:49 +0900

    remove unnecessary stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import VueComponent from "@/components/VueComponent.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <VueComponent />
   </main>
 </template>
